0f5e93c422f498ec64201777228718fbbc7b0d86
Jørgen P. Tjernø <jorgenpt@gmail.com>
2013-03-12 22:26:05 -0700

    Refactor script & reorganize manifest.

--- thismightbeawesome.user.js
+++ thismightbeawesome.user.js
@@ -40,16 +40,13 @@
       is done.
  *** Changelog ***/
 
-// Quick drop-out if we're on the wrong page
-if ( window.location.search.indexOf("c=votedetail") >= 0 ) {
+// Find the right script to run.
+var query = window.location.search;
+if (query.indexOf("c=votedetail") >= 0) {
     sumVotes();
-    return;
+} else if (query.indexOf("c=comments") >= 0) {
+    summarizeVotes();
 }
-var commentList = document.getElementById("commentList");
-if (!commentList)
-    return;
-
-String.prototype.trim = function() { return this.replace(/^\s+|\s+$/, ''); };
 
 // addition by oliphaunt: sum votes.
 function sumVotes() {
@@ -89,146 +86,154 @@ function sumVotes() {
     table.parentNode.insertBefore(div,table);
 }
 
-// Set scrolling to the element with the given name.
-function jumpToName(name)
-{
-    var anchors = document.getElementsByName(name);
-    if (!anchors.length)
+function summarizeVotes() {
+    var commentList = document.getElementById("commentList");
+    if (!commentList)
         return;
 
-    var element = anchors[0];
-    var offset = 0;
-    while (element)
+    String.prototype.trim = function() { return this.replace(/^\s+|\s+$/, ''); };
+
+    // Set scrolling to the element with the given name.
+    function jumpToName(name)
     {
-        offset += element.offsetTop;
-        element = element.offsetParent;
+        var anchors = document.getElementsByName(name);
+        if (!anchors.length)
+            return;
+
+        var element = anchors[0];
+        var offset = 0;
+        while (element)
+        {
+            offset += element.offsetTop;
+            element = element.offsetParent;
+        }
+
+        window.scroll(window.scrollX, offset);
     }
 
-    window.scroll(window.scrollX, offset);
-}
-
-var summaries = {};
-// Increment the counts for the votes that this user posted.
-function updateSummaries(entry)
-{
-    var links = entry.getElementsByClassName('userLink');
-    if (links.length < 1)
+    var summaries = {};
+    // Increment the counts for the votes that this user posted.
+    function updateSummaries(entry)
     {
-        GM_log("Couldn't find userLink for post, skipping.");
-        return;
+        var links = entry.getElementsByClassName('userLink');
+        if (links.length < 1)
+        {
+            GM_log("Couldn't find userLink for post, skipping.");
+            return;
+        }
+
+        var user = links[0];
+        var votes = entry.getElementsByClassName('vote');
+        for (var i = 0; i < votes.length; i++)
+        {
+            var vote = votes[i].textContent.trim();
+            if (!summaries[vote])
+                summaries[vote] = [];
+            summaries[vote].push(user);
+        }
     }
 
-    var user = links[0];
-    var votes = entry.getElementsByClassName('vote');
-    for (var i = 0; i < votes.length; i++)
+    function sortSummaries()
     {
-        var vote = votes[i].textContent.trim();
-        if (!summaries[vote])
-            summaries[vote] = [];
-        summaries[vote].push(user);
-    }
-}
+        function byUsername(a, b)
+        {
+            var anew = a.textContent.toLowerCase();
+            var bnew = b.textContent.toLowerCase();
+            if (anew < bnew) return -1;
+            if (anew > bnew) return 1;
+            return 0;
+        }
 
-function sortSummaries()
-{
-    function byUsername(a, b)
+        var sortable = [];
+        for (var summary in summaries)
+        {
+            var voters = summaries[summary];
+            voters.sort(byUsername);
+            sortable.push([summary, voters]);
+        }
+
+        sortable.sort();
+        summaries = {};
+        for (var i = 0; i < sortable.length; ++i)
+            summaries[sortable[i][0]] = sortable[i][1];
+    }
+
+    // Calculate the length of a users post - just the content.
+    function postLength(entry)
     {
-        var anew = a.textContent.toLowerCase();
-        var bnew = b.textContent.toLowerCase();
-        if (anew < bnew) return -1;
-        if (anew > bnew) return 1;
-        return 0;
+        var text = '';
+        for (var i = 0; i < entry.childNodes.length; ++i)
+        {
+            var child = entry.childNodes[i];
+            var type = child.nodeName;
+
+            // We count everything until the timestamp <div> (which is the first non-post-content element).
+            if (type != 'DIV' || child.className.indexOf('timestamp') == -1)
+                text += child.textContent.trim();
+            else
+                break;
+        }
+
+        return text.length;
     }
 
-    var sortable = [];
+    var colors = ['#CCF', '#BBE'];
+    var color = 0;
+
+    // Go over all the posts, remove empty ones and add them to the summary.
+    var entries = commentList.getElementsByClassName("entry");
+    for (var i = 0; i < entries.length; ++i)
+    {
+        entry = entries[i];
+
+        // Non-empty posts are left alone (but recolored, to keep colors).
+        if (postLength(entry))
+        {
+            entry.style.backgroundColor = colors[color];
+            color = (color + 1) % colors.length;
+        }
+        else // Empty posts are hidden.
+            entry.style.display = 'none';
+
+        // Update summaries based on this post.
+        updateSummaries(entry);
+    }
+
+    // Sort votes and voters.
+    sortSummaries();
+
+    // Create an <ul> of the different votes and their voters.
+    var ul = document.createElement('ul');
     for (var summary in summaries)
     {
         var voters = summaries[summary];
-        voters.sort(byUsername);
-        sortable.push([summary, voters]);
+        var li = document.createElement('li');
+
+        li.textContent = summary + ' ';
+        for (var i = 0; i < voters.length; i++)
+        {
+            li.appendChild(voters[i].cloneNode(true));
+            li.appendChild(document.createTextNode(', '));
+        }
+
+        // Remove last separator.
+        li.removeChild(li.childNodes[li.childNodes.length - 1]);
+
+        // Add a count of votes
+        if (voters.length > 3)
+            li.appendChild(document.createTextNode(' (' + voters.length + ' people)'));
+
+        ul.appendChild(li);
     }
 
-    sortable.sort();
-    summaries = {};
-    for (var i = 0; i < sortable.length; ++i)
-        summaries[sortable[i][0]] = sortable[i][1];
+    // Add it to the top of the comment list.
+    commentList.insertBefore(ul, entries[0].previousElementSibling);
+
+    // This makes it recalc the position after modifying DOM.
+    var hash = window.location.hash;
+    if (hash.indexOf('#') == 0)
+        hash = hash.substring(1);
+
+    if (hash.length)
+        jumpToName(hash);
 }
-
-// Calculate the length of a users post - just the content.
-function postLength(entry)
-{
-    var text = '';
-    for (var i = 0; i < entry.childNodes.length; ++i)
-    {
-        var child = entry.childNodes[i];
-        var type = child.nodeName;
-
-        // We count everything until the timestamp <div> (which is the first non-post-content element).
-        if (type != 'DIV' || child.className.indexOf('timestamp') == -1)
-            text += child.textContent.trim();
-        else
-            break;
-    }
-
-    return text.length;
-}
-
-var colors = ['#CCF', '#BBE'];
-var color = 0;
-
-// Go over all the posts, remove empty ones and add them to the summary.
-var entries = commentList.getElementsByClassName("entry");
-for (var i = 0; i < entries.length; ++i)
-{
-    entry = entries[i];
-
-    // Non-empty posts are left alone (but recolored, to keep colors).
-    if (postLength(entry))
-    {
-        entry.style.backgroundColor = colors[color];
-        color = (color + 1) % colors.length;
-    }
-    else // Empty posts are hidden.
-        entry.style.display = 'none';
-
-    // Update summaries based on this post.
-    updateSummaries(entry);
-}
-
-// Sort votes and voters.
-sortSummaries();
-
-// Create an <ul> of the different votes and their voters.
-var ul = document.createElement('ul');
-for (var summary in summaries)
-{
-    var voters = summaries[summary];
-    var li = document.createElement('li');
-
-    li.textContent = summary + ' ';
-    for (var i = 0; i < voters.length; i++)
-    {
-        li.appendChild(voters[i].cloneNode(true));
-        li.appendChild(document.createTextNode(', '));
-    }
-
-    // Remove last separator.
-    li.removeChild(li.childNodes[li.childNodes.length - 1]);
-
-    // Add a count of votes
-    if (voters.length > 3)
-        li.appendChild(document.createTextNode(' (' + voters.length + ' people)'));
-
-    ul.appendChild(li);
-}
-
-// Add it to the top of the comment list.
-commentList.insertBefore(ul, entries[0].previousElementSibling);
-
-// This makes it recalc the position after modifying DOM.
-var hash = window.location.hash;
-if (hash.indexOf('#') == 0)
-    hash = hash.substring(1);
-
-if (hash.length)
-    jumpToName(hash);
